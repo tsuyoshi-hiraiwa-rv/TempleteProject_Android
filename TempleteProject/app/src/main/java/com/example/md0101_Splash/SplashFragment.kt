6d1917782e969681aeed7e82f1c6231b6f5bde52
Tsuyoshi Hiraiwa <tsuyoshi.hiraiwa@mail.raven-g.com>
2022-04-23 20:44:11 +0900

    データバインディング成功時点

--- TempleteProject/app/src/main/java/com/example/md0101_Splash/SplashFragment.kt
+++ TempleteProject/app/src/main/java/com/example/md0101_Splash/SplashFragment.kt
@@ -2,11 +2,17 @@ package com.example.md0101_Splash
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.os.Environment
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+
+// データバインディングを実現するために必要なライブラリ
+import androidx.databinding.DataBindingUtil
+
 import com.example.templeteproject.R
+import com.example.templeteproject.databinding.SplashFragmentBinding
 
 class SplashFragment : Fragment() {
 
@@ -14,19 +20,40 @@ class SplashFragment : Fragment() {
         fun newInstance() = SplashFragment()
     }
 
-    private lateinit var viewModel: SplashViewModel
+    private lateinit var myViewModel: SplashViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.splash_fragment, container, false)
-    }
+    ) =
+        SplashFragmentBinding.inflate(inflater).also {
+            it.lifecycleOwner = this
+            this.myViewModel = ViewModelProvider(this).get(SplashViewModel::class.java)
+            it.viewModel = this.myViewModel
+        }.root
+//    ): View? {
+//        // データバインディングの初期設定
+//        // DataBindingUtilからBindingインスタンスを生成
+//        val binding = DataBindingUtil.inflate<SplashFragmentBinding>(inflater, R.layout.splash_fragment, container, false);
+//        // ライフサイクルオーナにActivityを指定しないとデータの管理が始まらない
+//        binding.lifecycleOwner = this
+//        myViewModel = ViewModelProvider(this).get(SplashViewModel::class.java)
+//        binding.viewModel = myViewModel
+//
+////        this.initBinding()
+//        return inflater.inflate(R.layout.splash_fragment, container, false)
+//    }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(SplashViewModel::class.java)
+        myViewModel = ViewModelProvider(this).get(SplashViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
+    /**
+     * データバインディングの初期設定
+     */
+    private fun initBinding() {
+
+    }
 }
--- TempleteProject/app/src/main/java/com/example/md0101_Splash/SplashViewModel.kt
+++ TempleteProject/app/src/main/java/com/example/md0101_Splash/SplashViewModel.kt
@@ -1,7 +1,16 @@
 package com.example.md0101_Splash
 
+// コンソールログの出力に必要なライブラリ
+import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class SplashViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    // この画面の表示タイトル
+    val myTitle: String = "Splash"
+
+    // Nextボタンをタップしたときの処理
+    fun onclickNextButton() {
+        // とりあえず画面遷移
+        Log.d(this.javaClass.name, "NEXTボタンタップ")
+    }
 }
